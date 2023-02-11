e458748fd253d893cd973997b4a73c8002990581
vboxuser <vboxuser@WINDA10>
2023-02-11 11:11:26 +0500

    Добавьте файлы проекта.

---
+++ dvumernie massivi/dvumernie massivi.cpp
@@ -0,0 +1,50 @@
+﻿// dvumernie massivi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    srand(time(NULL));
+    int  n, m, p, q,b;
+
+    cin >> n;
+    cin >> m;
+
+
+    int a[100][100];
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int v = 0; v < m; v++)
+        {
+            a[k][v] = 1 + rand() % 9;
+            cout << a[k][v] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\n";
+
+    cin >> p;
+    cin >> q;
+
+    
+
+    for (int i = 0; i < n; i++) {
+        b = a[i][p];
+        a[i][p] = a[i][q];
+        a[i][q] = b;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+
+
+}
---
+++ dvumernie massivi2/dvumernie massivi2.cpp
@@ -0,0 +1,49 @@
+﻿// dvumernie massivi2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+    srand(time(NULL));
+    int  n;
+    bool sim;
+
+    cin >> n;
+    
+
+
+    int a[10][10];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            a[i][j] = 1 + rand() % 9;
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\n";
+
+    sim = true;
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            if (a[i][j] != a[j][i]) 
+                sim = false;
+        }
+    }
+    
+    if (sim) cout << "YES";
+    else cout << "NO";
+   // в основном будет NO, т.к. числа рандомные, должно оооочень повезти, чтобы было YES.
+
+}
+
+
